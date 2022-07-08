bc7cc0fed8aaec16348d12de6f53ae2515747fd6
Anton.R <Anton-R@live.com>
2022-07-08 08:20:18 +0300

    crud updated

--- src/handlers/api.py
+++ src/handlers/api.py
@@ -5,27 +5,27 @@ from fastapi import APIRouter
 logger = logging.getLogger(__name__)
 api_router = APIRouter()
 
-# http://localhost:8001/api/get_users
-@api_router.get('/get_users')
+# http://localhost:8001/api/user
+@api_router.get('/user')
 def get_users():
     logger.info('getting hello world')
     return {'msg': 'get_users'}
 
 
-@api_router.get('/get_user/{name}')
+@api_router.get('/user/{name}')
 def get_user(name: str):
     logger.info('getting hello world')
     return {'msg': f'get user {name}'}
 
 
 
-@api_router.post('/create_user')
+@api_router.post('/user')
 def create_user():
     logger.info('getting hello world')
     return {'msg': 'creating user'}
 
 
-@api_router.patch('/create_user')
+@api_router.patch('/user')
 def update_user():
     logger.info('getting hello world')
     return {'msg': 'updating user'}
