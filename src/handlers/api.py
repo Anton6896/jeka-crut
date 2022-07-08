ecd79789b89bab06b4cd19325cea2383406809bc
Anton.R <Anton-R@live.com>
2022-07-08 08:18:24 +0300

    cud api

--- src/handlers/api.py
+++ src/handlers/api.py
@@ -5,8 +5,27 @@ from fastapi import APIRouter
 logger = logging.getLogger(__name__)
 api_router = APIRouter()
 
-# http://localhost:8001/api/hello
-@api_router.get("/hello")
-def read_root():
+# http://localhost:8001/api/get_users
+@api_router.get('/get_users')
+def get_users():
     logger.info('getting hello world')
-    return {"Hello": "World"}
+    return {'msg': 'get_users'}
+
+
+@api_router.get('/get_user/{name}')
+def get_user(name: str):
+    logger.info('getting hello world')
+    return {'msg': f'get user {name}'}
+
+
+
+@api_router.post('/create_user')
+def create_user():
+    logger.info('getting hello world')
+    return {'msg': 'creating user'}
+
+
+@api_router.patch('/create_user')
+def update_user():
+    logger.info('getting hello world')
+    return {'msg': 'updating user'}
